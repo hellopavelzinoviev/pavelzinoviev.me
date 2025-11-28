d43e10cf3bfa382648d93f1d05128e6681eba82f
Pavel Zinoviev <hello.pavel.zinoviev@gmail.com>
2025-11-29 00:14:14 +0400

    chore: few elements

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,26 @@
 <template>
-  <section>
-    <h1>
-      <a href="https://bsky.app/profile/pavelzinoviev.me" target="_blank">pavel zinoviev</a>
-    </h1>
-    <div>
-      <h2>to do:</h2>
-      <ul>
-        <li aria-checked="true"><span aria-hidden="true">-[x]</span> buy a domain</li>
-        <li aria-checked="false"><span aria-hidden="true">-[ ]</span> design the site</li>
-        <li aria-checked="false"><span aria-hidden="true">-[ ]</span> build the site</li>
-      </ul>
-    </div>
-  </section>
+  <main>
+    <header>
+      <span>✷✷✷</span>
+    </header>
+    <section>
+      <h1>
+        <a href="https://bsky.app/profile/pavelzinoviev.me" target="_blank">pavel zinoviev</a>
+      </h1>
+      <div>
+        <h2>to do:</h2>
+        <ul>
+          <li aria-checked="true"><span aria-hidden="true">-[x]</span> buy a domain</li>
+          <li aria-checked="false"><span aria-hidden="true">-[ ]</span> design the site</li>
+          <li aria-checked="false"><span aria-hidden="true">-[ ]</span> build the site</li>
+        </ul>
+      </div>
+    </section>
+    <footer>
+      <span>2025</span>
+      <span>✷✷✷</span>
+    </footer>
+  </main>
 </template>
 
 <style>
@@ -19,11 +28,23 @@ body {
   margin: 0;
   font-family: monospace;
 }
-section {
+main {
   display: flex;
   flex-direction: column;
   align-items: center;
   height: 100vh;
+}
+section {
+  flex-grow: 1;
+}
+footer,
+header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
+  padding: 2em;
 }
 h1 {
   margin-top: 20vw;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
-})
+  ssr: false,
+
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+});
